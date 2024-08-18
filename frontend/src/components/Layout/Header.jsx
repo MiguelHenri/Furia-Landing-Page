120ece5847d63b9a3ca766f836ccf584e56a5376
MiguelHenri <miguelbrahen@gmail.com>
2024-08-18 12:19:20 -0300

    beginning to build header

--- frontend/src/components/Layout/Header.jsx
+++ frontend/src/components/Layout/Header.jsx
@@ -1,10 +1,30 @@
-import { Text } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Anchor, Burger, Group, Image } from "@mantine/core";
+import { IconShoppingBag } from '@tabler/icons-react';
 
 function Header() {
+
+    const [opened, {toggle}] = useDisclosure();
+
+    const data = [
+        { link: '', label: 'Loja'},
+        { link: '', label: 'Times'},
+        { link: '', label: 'Agenda'},
+        { link: '', label: 'Parceiros'},
+        { link: '', label: 'Contato'},
+      ];
+
     return (
-        <Text>
-            Header.
-        </Text>
+        <Group justify="space-between" h={70}>
+            <Burger opened={opened} onClick={toggle} size="md" ml={30}/>
+            <Image 
+                src='https://upload.wikimedia.org/wikipedia/pt/f/f9/Furia_Esports_logo.png'
+                h={60}
+            />
+            <Anchor mr={30} c='black'>
+                <IconShoppingBag stroke={2} width={30} height={30}/>
+            </Anchor>
+        </Group>
     )
 }
 
--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -1,9 +1,12 @@
 import { Flex, Box } from "@mantine/core";
 import { Outlet } from "react-router-dom";
+import Header from "../components/Layout/Header";
 
 function Layout() {
     return (
         <Flex h="100vh" direction="column" justify='flex-start'>
+
+            <Header/>
 
             <Box style={{flex:1}}> 
                 <Outlet/>
